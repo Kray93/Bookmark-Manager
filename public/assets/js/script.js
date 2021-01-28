4110f9f9b08cb151404d9b18a66e005fb5052c1f
Zach <zdspringer99@gmail.com>
2021-01-28 11:51:38 -0800

    pseudo code added

---
+++ public/assets/js/script.js
@@ -0,0 +1,15 @@
+// TODO: document.ready(function())
+// TODO: function to create new account
+// TODO: function to edit account info
+// TODO: function to create tab
+// TODO: function to create BM
+// TODO: function to create collection
+// TODO: function to display tab
+// TODO: function to display BM
+// TODO: function to display collection
+// TODO: function to edit tab
+// TODO: function to edit BM
+// TODO: function to edit collection
+// TODO: function to delete tab
+// TODO: function to delete BM
+// TODO: function to delete collection
