033fc7d131d29575e38f5f6c7a0e4e6bfee4fa1b
Zach <zdspringer99@gmail.com>
2021-01-28 13:38:20 -0800

    added function names and organized them into sections

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,38 @@
 // TODO: document.ready(function())
+// each will have an ajax call with .then and callback
+
+//Create Functions=========================================
 // TODO: function to create new account
-// TODO: function to edit account info
+function createNewAcc()
 // TODO: function to create tab
+function createTab()
 // TODO: function to create BM
+function createBM()
 // TODO: function to create collection
+function createCollect()
+
+//Display Functions========================================
 // TODO: function to display tab
+function displayTab()
 // TODO: function to display BM
+function displayBM()
 // TODO: function to display collection
+function displayCollect()
+
+//Edit Functions==========================================
+// TODO: function to edit account info
+function editAccInfo()
 // TODO: function to edit tab
+function editTab()
 // TODO: function to edit BM
+function editBM()
 // TODO: function to edit collection
+function editCollect()
+
+//Delete Functions=======================================
 // TODO: function to delete tab
+function deleteTab()
 // TODO: function to delete BM
+function deleteBM()
 // TODO: function to delete collection
+function deleteCollect()
