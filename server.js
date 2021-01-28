2c52eabb1a00a7d49705824b1cdd31d0bbf0895c
JohnDJake <john.d.jake@gmail.com>
2021-01-28 12:46:24 -0800

    forgot to uncomment some lines

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// const db = require('./models');
+const db = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,8 +12,8 @@ app.use(express.static(__dirname + "/public"));
 app.use("/assets/d3", express.static(__dirname + "/node_modules/d3/dist"));
 app.use("/assets/materialize", express.static(__dirname + "/node_modules/materialize-css/dist"));
 
-// db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
-// });
+});
