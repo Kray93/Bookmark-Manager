9ec6b09ae454403131bf94cce42c967a2ff7a3eb
mtrupiano <markt6749@gmail.com>
2021-01-29 13:53:30 -0800

    brainstorming tag controller

---
+++ controllers/tagController.js
@@ -0,0 +1,18 @@
+// GETS
+// get all tags
+
+// get all tags that are attached to a bookmark
+
+// POSTS
+// create a new tag, attach to a bookmark
+
+// add an existing tag to an existing bookmark
+
+// PUTS
+// rename a tag
+
+// DELETES
+// Delete a user's tag
+
+// Delete a tag from a bookmark
+
