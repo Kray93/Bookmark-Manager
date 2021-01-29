58f6020cbfd8d976294209c677d29340e7384654
mtrupiano <markt6749@gmail.com>
2021-01-29 12:47:42 -0800

    spacing and var name edits

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,54 +5,55 @@ const bcrypt = require("bcrypt");
 
 router.post("/signup", function(request, response) {
     db.User.create({
-        username: req.body.username,
-        password: req.body.password
+        username: request.body.username,
+        password: request.body.password
     }).then( (result) => {
         res.json(result);
     }).catch( (err) => {
         res.status(500).json(err);
     })
-})
-router.post("/login", (req, res) => {
+});
+
+router.post("/login", function(request, response) {
     db.User.findOne({
         where: {
-            username: req.body.username
+            username: request.body.username
         }
     }).then(userData => {
         if (!userData) {
-            req.session.destroy();
-            res.status(404).send("no such user")
+            request.session.destroy();
+            response.status(404).send("no such user")
         } else {
-            if (bcrypt.compareSync(req.body.password, userData.password)) {
-                req.session.user = {
+            if (bcrypt.compareSync(request.body.password, userData.password)) {
+                request.session.user = {
                     id: userData.id,
                     username: userData.username
                 }
-                res.json(userData);
+                response.json(userData);
             } else {
-                req.session.destroy();
-                res.status(401).send("wrong password bro")
+                request.session.destroy();
+                response.status(401).send("wrong password bro")
             }
         }
-    })
-})
+    });
+});
 
-router.get("/readsessions", (req, res) => {
-    res.json(req.session)
-})
+router.get("/readsessions", (request, response) => {
+    response.json(request.session)
+});
 
-router.get("/secretclub", (req, res) => {
-    if (req.session.user) {
-        res.send(`welcome to the club, ${req.session.user.username}!`)
+router.get("/secretclub", (request, response) => {
+    if (request.session.user) {
+        response.send(`welcome to the club, ${request.session.user.username}!`)
     } else {
-        res.status(401).send("login first you knucklehead")
+        response.status(401).send("login first you knucklehead")
     }
-})
+});
 
-router.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/')
-})
+router.get('/logout', (request, response) => {
+    request.session.destroy();
+    response.redirect('/')
+});
 
 
 module.exports = router;
