ff2e7c3e879045150be17af69c9e150551cb439f
Zach <zdspringer99@gmail.com>
2021-01-31 15:41:09 -0800

    added temporary route for testing purposes

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const db = require('./models');
+// const db = require('./models');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(__dirname + "/public"));
-app.use("/assets/materialize", express.static(__dirname + "/node_modules/materialize-css/dist"));
-app.use("/assets/jquery", express.static(__dirname + "/node_modules/jquery/dist"));
+app.use(
+  "/assets/materialize",
+  express.static(__dirname + "/node_modules/materialize-css/dist")
+);
+app.use(
+  "/assets/jquery",
+  express.static(__dirname + "/node_modules/jquery/dist")
+);
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -19,8 +25,12 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
-        console.log("App listening on PORT " + PORT);
-    });
+app.get("/", function (req, res) {
+  res.render("index", { bookmarks: [] });
 });
+
+// db.sequelize.sync({ force: true }).then(function() {
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+// });
