e25afe9db85b442866b0ef211f2472359373c8ce
JohnDJake <john.d.jake@gmail.com>
2021-02-01 14:18:59 -0800

    placeholder data to render

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use(express.json());
 
 app.use(express.static(__dirname + "/public"));
 app.use(
-  "/assets/materialize",
-  express.static(__dirname + "/node_modules/materialize-css/dist")
+    "/assets/materialize",
+    express.static(__dirname + "/node_modules/materialize-css/dist")
 );
 app.use(
-  "/assets/jquery",
-  express.static(__dirname + "/node_modules/jquery/dist")
+    "/assets/jquery",
+    express.static(__dirname + "/node_modules/jquery/dist")
 );
 
 // Set Handlebars.
@@ -26,11 +26,11 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.get("/", function (req, res) {
-  res.render("index", { bookmarks: [] });
+    res.render("index", { name: "my bookmarks", collections: [{ name: "work", id: 1, color: "red", collections: [{ name: "css", color: "salmon", bookmarks: [{ name: "W3 Schools CSS", id: 1, url: "https://www.w3schools.com/css/default.asp" }] }], bookmarks: [{ name: "BCS", id: 2, url: "bootcampspot.com" }] }], bookmarks: [] });
 });
 
 // db.sequelize.sync({ force: true }).then(function() {
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
 // });
