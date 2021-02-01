01c34fe009817e6ad34563c23cbf99c0ad967f9c
Zach <zdspringer99@gmail.com>
2021-02-01 12:40:25 -0800

    initial crud routes set up for collections and bm's

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,260 +1,389 @@
 // TODO: document.ready(function())
 // each will have an ajax call with .then and callback
 
+const { data } = require("jquery");
+
 //Create Functions=========================================
 // TODO: function to create new account
 // function createNewAcc()
 // TODO: function to create tab
 // function createTab()
-// TODO: function to create BM
-// function createBM()
-// TODO: function to create collection
-// function createCollect()
+
+// function to create BM
+
+function createBM(name, url, comment, color, collection, callback) {
+  $.post("/api/bookmarks", { data: { name, url, comment, color, collection } })
+    .then(callback)
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+// function to create collection
+
+function createCollect(name, color, parent, callback) {
+  $.post("/api/collections/", { data: { name, color, parent } })
+    .then(callback)
+    .fail((err) => {
+      console.log(err);
+    });
+}
 
 //Display Functions========================================
 // TODO: function to display everything in a tab
 
-// function displayTabs(call) {
-// $.get("/api/collections").then(call).fail((err) => { console.log(err) })
-// }
+function displayTabs(call) {
+  $.get("/api/collections")
+    .then(call)
+    .fail((err) => {
+      console.log(err);
+    });
+}
 
-// TODO: function to display BM
-// function displayBM()
+// function to display BM
+
+function displayBM(data, bm) {
+  if (data.id) {
+    $.ajax({
+      method: "GET",
+      url: "/api/bookmarks/?id=" + data.id,
+    })
+      .then(bm)
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+  if (data.color || data.collection) {
+    $.get(
+      "/api/bookmarks/?collection= " + data.collection,
+      "/api/bookmarks/?color=" + data.color
+    )
+      .then(bm)
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+  if (data.tag) {
+    $.get("/api/bookmarks/?tag=" + data.tag)
+      .then(bm)
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+}
+
+function displayuncatBM(uncatBM) {
+    if(uncatBM)
+    $.ajax({
+        method: "GET",
+        url: "/api/bookmarks/uncategorized"
+    })
+}
+
 // TODO: function to display collection
-// function displayCollect() {
 
-// }
+function displayCollect(displaysub) {
+  $.get("/api/collection/subcollections")
+    .then(displaysub)
+    .fail((err) => {
+      console.log(err);
+    });
+}
 
 //Edit Functions==========================================
 // TODO: function to edit account info
 // function editAccInfo()
 // TODO: function to edit tab
 // function editTab()
-// TODO: function to edit BM
-// function editBM()
-// TODO: function to edit collection
-// function editCollect()
+
+// function to edit BM name, url, comment, and color
+
+function editBM(data) {
+  if (data.newName) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/collections/name",
+      data: { newName: data.newName, id: data.id },
+    })
+      .then()
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+  if (data.newURL) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/bookmarks/url",
+      data: { newURL: data.url, id: data.id },
+    })
+      .then()
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+  if (data.newComment) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/bookmarks/comment",
+      data: { newComment: data.comment, id: data.id },
+    })
+      .then()
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+  if (data.newColor) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/collections/color",
+      data: { newColor: data.color, id: [data.id] },
+    })
+      .then()
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+}
+
+// function to edit collection name, color, and parent
+
+function editCollect(data) {
+  if (data.newName) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/collections/name",
+      data: { newName: data.newName, id: data.id },
+    })
+      .then()
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+  if (data.newColor) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/collections/color",
+      data: { newColor: data.color, id: data.id },
+    })
+      .then()
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+  if (data.newParent) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/collections/parent",
+      data: { newParent: data.parent, id: data.id },
+    })
+      .then()
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+}
 
 //Delete Functions=======================================
 // TODO: function to delete tab
 // function deleteTab()
-// TODO: function to delete BM
-// function deleteBM()
-// TODO: function to delete collection
-// function deleteCollect()
+
+// function to delete single BM
+
+function deleteBM() {
+  $.ajax({
+    method: "DELETE",
+    url: "/api/bookmarks/:id",
+  })
+    .then()
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+// function to delete collection
+
+function deleteCollect() {
+  $.ajax({
+    method: "DELETE",
+    url: "/api/collection/:id",
+  })
+    .then()
+    .fail((err) => {
+      console.log(err);
+    });
+}
+
+//Move Functions============================================
+// TODO: function to move a bm from one collection to another
+
+function moveBM(newCollection, originalCollection, id, deleteFromOriginalCollection){
+    $.ajax({
+        method: "PUT",
+        url: "/api/bookmarks/collection",
+        data: { 
+            newCollection: ,
+            originalCollection: ,
+            id: ,
+            deleteFromOriginalCollection:
+         }
+    }).then()
+    .fail((err) => {
+      console.log(err);
+    });
+}
 
 // TODO: set up click event handlers for links to get subcollections
 // TODO: create next menu for those
 
-function start(tabs) {
-  const div = $("<div>");
-  $(document.body).append(div);
-  var links = tabs.map((tab) => ({
-    label: tab.name,
-    bg: tab.color,
-    id: tab.id,
-  }));
-  var windowHeight = window.innerHeight;
-  if (windowHeight === 0) windowHeight = 238;
-  var radius = windowHeight * 0.6, //sets the radius to react to the window
-    circle = $("<div>"),
-    borderSize = radius * 0.021,
-    totalArea = 48,
-    increment = totalArea / (links.length - 1),
-    startPoint = 0 - totalArea / 2,
-    fontSize = radius * 0.12,
-    linkSize = radius * 0.25;
-  styleCircle();
-  addCircle();
-  addLinks();
-  styleLinks();
-  function styleCircle() {
-    circle.css({
-      // circle.style.border = borderSize + "px solid #fff";
-      border: borderSize + "px solid #fff",
-      // circle.style.width = radius * 2 + "px";
-      width: radius * 2 + "px",
-      // circle.style.height = radius * 2 + "px";
-      height: radius * 2 + "px",
-      // circle.style.borderRadius = radius + "px";
-      borderRadius: radius + "px",
-      // circle.style.position = "absolute";
-      position: "absolute",
-      // circle.style.top = "-" + radius * 0.2 + "px";
-      top: "-" + radius * 0.2 + "px",
-      // circle.style.left = radius * -1 + "px";
-      left: radius * -1 + "px",
-    });
-  }
-  function addCircle() {
-    // document.body.appendChild(circle);
-    div.append(circle);
-  }
-  function addLinks() {
-    for (var i = 0, l = links.length; i < l; i++) {
-      //set data-id attr to links[i].id
-      //   (link = document.createElement("a")),
-      link = $("<a>");
-      //     (hover = document.createElement("span"));
-      hover = $("<span>");
-      //   link.href = "#";
-      link.attr("href", "#");
-      //   link.dataset.color = links[i].bg;
-      link.data("color", links[i].bg);
-      link.css({
-        //   link.style.display = "inline-block";
-        display: "inline-block",
-        //   link.style.textDecoration = "none";
-        textDecoration: "none",
-        //   link.style.color = "#fff";
-        color: "#fff",
-        //   link.style.position = "absolute";
-        position: "absolute",
-        //   link.style.zIndex = 100;
-        zIndex: 100,
-      });
-      //   link.innerHTML = links[i].label;
-      link.text(links[i].label);
-      //   hover.style.position = "absolute";
-      hover.css({
-        position: "absolute",
-        //   hover.style.display = "inline-block";
-        display: "inline-block",
-        //   hover.style.zIndex = 50;
-        zIndex: 50,
-        //   hover.style.opacity = 0;
-        oppacity: 0,
-      });
-      //   document.body.appendChild(link);
-      div.append(
-        link,
-        //   document.body.appendChild(hover);
-        hover
-      );
-      //   link.addEventListener("mouseover", linkOver);
-      link.on("mouseover", linkOver);
-      //   link.addEventListener("mouseout", linkOut);
-      link.on("mouseout", linkOut);
-      links[i].elem = link;
-      links[i].hover = hover;
-    }
-  }
-  function styleLinks() {
-    for (var i = 0, l = links.length; i < l; i++) {
-      var link = links[i].elem,
-        hover = links[i].hover,
-        deg = startPoint + i * increment;
-      //   link.style.paddingLeft = radius * 1.2 + "px";
-      link.css({
-        paddingLeft: radius * 1.2 + "px",
-        //   link.style.fontSize = fontSize + "px";
-        fontSize: (fontSize = "px"),
-        //   link.style.height = linkSize + "px";
-        height: linkSize + "px",
-        //   link.style.lineHeight = linkSize + "px";
-        lineHeight: linkSize + "px",
-        //   link.style.left = borderSize + "px";
-        left: borderSize + "px",
-        //   link.style.top = windowHeight / 2 - windowHeight * 0.1 + borderSize + "px";
-        top: windowHeight / 2 - windowHeight * 0.1 + borderSize + "px",
-      });
-      setTransformOrigin(link, "0px " + linkSize * 0.5 + "px");
-      setTransition(link, "all 0.2s ease-out");
-      setTransform(link, "rotate(" + deg + "deg)");
-      hover.css({
-        // hover.style.left = borderSize + "px";
-        left: borderSize + "px",
-        // hover.style.top = windowHeight * 0.4 + borderSize + "px";
-        top: windowHeight * 0.4 + borderSize + "px",
-        // hover.style.width = radius + borderSize / 2 + "px";
-        width: radius + borderSize / 2 + "px",
-        // hover.style.height = linkSize + "px";
-        height: linkSize + "px",
-        // hover.style.borderRight = borderSize * 2 + "px solid #fff";
-        borderRight: borderSize * 2 + "px solid #fff",
-      });
-      setTransformOrigin(hover, "0px " + linkSize * 0.5 + "px");
-      setTransition(hover, "all 0.2s ease-out");
-      setTransform(hover, "rotate(" + deg + "deg)");
-    }
-  }
-  window.onresize = function () {
-    windowHeight = window.innerHeight;
-    (radius = windowHeight * 0.6), (borderSize = radius * 0.021);
-    (fontSize = radius * 0.12), (linkSize = radius * 0.25);
-    styleCircle();
-    styleLinks();
-  };
-  function linkOver(e) {
-    var thisLink = $(e.target),
-      thisHover = $(e.target.nextSibling);
-    // thisLink.style.paddingLeft = radius * 1.25 + "px";
-    thisLink.css("paddingLeft", radius * 1.25 + "px");
-    // thisHover.style.opacity = 1;
-    thisHover.css("opacity", 1);
-    // document.body.style.backgroundColor = thisLink.dataset.color;
-    $(document.body).css("backgroundColor", thisLink.data("color"));
-  }
-  function linkOut(e) {
-    var thisLink = $(e.target),
-      thisHover = $(e.target.nextSibling);
-    // thisLink.style.paddingLeft = radius * 1.2 + "px";
-    thisLink.css("paddingLeft", radius * 1.2 + "px");
-    // thisHover.style.opacity = 0;
-    thisHover.css("opacity", 0);
-  }
-  function setTransform(element, string) {
-    element.css({
-      // element.style.webkitTransform = string;
-      webkitTransform: string,
-      // element.style.MozTransform = string;
-      MozTransform: string,
-      // element.style.msTransform = string;
-      msTransform: string,
-      // element.style.OTransform = string;
-      OTransform: string,
-      // element.style.transform = string;
-      transform: string,
-    });
-  }
-  function setTransformOrigin(element, string) {
-    element.css({
-      // element.style.webkitTransformOrigin = string;
-      webkitTransformOrigin: string,
-      // element.style.MozTransformOrigin = string;
-      MozTransformOrigin: string,
-      // element.style.msTransformOrigin = string;
-      msTransformOrigin: string,
-      // element.style.OTransformOrigin = string;
-      OTransformOrigin: string,
-      // element.style.transformOrigin = string;
-      transformOrigin: string,
-    });
-  }
-  function setTransition(element, string) {
-    element.css({
-      // element.style.webkitTransition = string;
-      webkitTransition: string,
-      // element.style.MozTransition = string;
-      MozTransition: string,
-      // element.style.msTransition = string;
-      msTransition: string,
-      // element.style.OTransition = string;
-      OTransition: string,
-      // element.style.transition = string;
-      transition: string,
-    });
-  }
-}
+// function start(tabs) {
+//   const div = $("<div>");
+//   $(document.body).append(div);
+//   var links = tabs.map((tab) => ({
+//     label: tab.name,
+//     bg: tab.color,
+//     id: tab.id,
+//   }));
+//   var windowHeight = window.innerHeight;
+//   if (windowHeight === 0) windowHeight = 238;
+//   var radius = windowHeight * 0.6, //sets the radius to react to the window
+//     circle = $("<div>"),
+//     borderSize = radius * 0.021,
+//     totalArea = 48,
+//     increment = totalArea / (links.length - 1),
+//     startPoint = 0 - totalArea / 2,
+//     fontSize = radius * 0.12,
+//     linkSize = radius * 0.25;
+//   styleCircle();
+//   addCircle();
+//   addLinks();
+//   styleLinks();
+//   function styleCircle() {
+//     circle.css({
+//       border: borderSize + "px solid #fff",
+//       width: radius * 2 + "px",
+//       height: radius * 2 + "px",
+//       borderRadius: radius + "px",
+//       position: "absolute",
+//       top: "-" + radius * 0.2 + "px",
+//       left: radius * -1 + "px",
+//     });
+//   }
+//   function addCircle() {
+//     div.append(circle);
+//   }
+//   function addLinks() {
+//     for (var i = 0, l = links.length; i < l; i++) {
+//       //set data-id attr to links[i].id
+//       link = $("<a>");
+//       hover = $("<span>");
+//       link.attr("href", "#");
+//       link.data("color", links[i].bg);
+//       link.css({
+//         display: "inline-block",
+//         textDecoration: "none",
+//         color: "#fff",
+//         position: "absolute",
+//         zIndex: 100,
+//       });
+//       link.text(links[i].label);
+//       hover.css({
+//         position: "absolute",
+//         display: "inline-block",
+//         zIndex: 50,
+//         oppacity: 0,
+//       });
+//       div.append(link, hover);
+//       link.on("mouseover", linkOver);
+//       link.on("mouseout", linkOut);
+//       links[i].elem = link;
+//       links[i].hover = hover;
+//     }
+//   }
+//   function styleLinks() {
+//     for (var i = 0, l = links.length; i < l; i++) {
+//       var link = links[i].elem,
+//         hover = links[i].hover,
+//         deg = startPoint + i * increment;
+//       link.css({
+//         paddingLeft: radius * 1.2 + "px",
+//         fontSize: (fontSize = "px"),
+//         height: linkSize + "px",
+//         lineHeight: linkSize + "px",
+//         left: borderSize + "px",
+//         top: windowHeight / 2 - windowHeight * 0.1 + borderSize + "px",
+//       });
+//       setTransformOrigin(link, "0px " + linkSize * 0.5 + "px");
+//       setTransition(link, "all 0.2s ease-out");
+//       setTransform(link, "rotate(" + deg + "deg)");
+//       hover.css({
+//         left: borderSize + "px",
+//         top: windowHeight * 0.4 + borderSize + "px",
+//         width: radius + borderSize / 2 + "px",
+//         height: linkSize + "px",
+//         borderRight: borderSize * 2 + "px solid #fff",
+//       });
+//       setTransformOrigin(hover, "0px " + linkSize * 0.5 + "px");
+//       setTransition(hover, "all 0.2s ease-out");
+//       setTransform(hover, "rotate(" + deg + "deg)");
+//     }
+//   }
+//   window.onresize = function () {
+//     windowHeight = window.innerHeight;
+//     (radius = windowHeight * 0.6), (borderSize = radius * 0.021);
+//     (fontSize = radius * 0.12), (linkSize = radius * 0.25);
+//     styleCircle();
+//     styleLinks();
+//   };
+//   function linkOver(e) {
+//     var thisLink = $(e.target),
+//       thisHover = $(e.target.nextSibling);
+//     thisLink.css("paddingLeft", radius * 1.25 + "px");
+//     thisHover.css("opacity", 1);
+//     $(document.body).css("backgroundColor", thisLink.data("color"));
+//   }
+//   function linkOut(e) {
+//     var thisLink = $(e.target),
+//       thisHover = $(e.target.nextSibling);
+//     thisLink.css("paddingLeft", radius * 1.2 + "px");
+//     thisHover.css("opacity", 0);
+//   }
+//   function setTransform(element, string) {
+//     element.css({
+//       webkitTransform: string,
+//       MozTransform: string,
+//       msTransform: string,
+//       OTransform: string,
+//       transform: string,
+//     });
+//   }
+//   function setTransformOrigin(element, string) {
+//     element.css({
+//       webkitTransformOrigin: string,
+//       MozTransformOrigin: string,
+//       msTransformOrigin: string,
+//       OTransformOrigin: string,
+//       transformOrigin: string,
+//     });
+//   }
+//   function setTransition(element, string) {
+//     element.css({
+//       webkitTransition: string,
+//       MozTransition: string,
+//       msTransition: string,
+//       OTransition: string,
+//       transition: string,
+//     });
+//   }
+// }
 
-function displayTabs(callback) {
-  callback([
-    { name: "work", color: "pink", id: 1 },
-    { name: "school", color: "black", id: 2 },
-    { name: "home", color: "red", id: 3 },
-  ]);
-}
+// function displayTabs(callback) {
+//   callback([
+//     { name: "work", color: "pink", id: 1 },
+//     { name: "school", color: "black", id: 2 },
+//     { name: "home", color: "red", id: 3 },
+//   ]);
+// }
 
-window.addEventListener("load", () => {
-  displayTabs(start);
-});
+// window.addEventListener("load", () => {
+//   displayTabs(start);
+// });
