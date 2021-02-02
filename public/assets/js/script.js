d1589c6c00b3a409bb88a492716a8d6f5a5f8fd6
Zach <zdspringer99@gmail.com>
2021-02-01 17:30:11 -0800

    bm, collection, tag create form functions done along with the edit and create tag functions

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,15 @@
 //Create Functions=========================================
 // TODO: function to create new account
 // function createNewAcc()
-// TODO: function to create tab
-// function createTab()
+// function to create tag
+
+function createTag(name, color, callback) {
+  $.post("/api/bookmarks/tags", { data: { name, color } })
+    .then(callback)
+    .fail((err) => {
+      console.log(err);
+    });
+}
 
 // function to create BM
 
@@ -22,6 +29,85 @@ function createCollect(name, color, parent, callback) {
     .fail((err) => {
       console.log(err);
     });
+}
+
+// function for create tag form
+
+function createTagForm(id) {
+  displayTabs({ id }, (tag) => {
+    $("input[name=tag]").val(tag.name);
+    $("input[name=tagcolor]").val(tag.color);
+    $("#tagEditForm").on("submit", (event) => {
+      event.preventDefault();
+      const createdTag = { id: tag.id };
+      if (tag.name !== $("input[name=tag]").val()) {
+        createdTag.newName = $("input[name=tag]").val();
+      }
+      if (tag.color !== $("input[name=tagcolor]").val()) {
+        createdTag.newColor = $("input[name=tagcolor]").val();
+      }
+      createTagForm(createdTag, location.reload);
+    });
+    $("#tagEditForm").show();
+  });
+}
+
+// function for create BM form
+
+function createBMForm(id) {
+  displayBM({ id }, (bm) => {
+    $("input[name=bookmark]").val(bm.name);
+    $("input[name=bmurl]").val(bm.url);
+    $("input[name=bmcolor]").val(bm.color);
+    $("input[name=bmtags]").val(bm.tags);
+    $("#create-bookmark").on("submit", (event) => {
+      event.preventDefault();
+      const createdBM = { id: bm.id };
+      if (bm.name !== $("input[name=bookmark]").val()) {
+        createdBM.newName = $("input[name=bookmark]").val();
+      }
+      if (bm.url !== $("input[name=bmurl]").val()) {
+        createdBM.newURL = $("input[name=bmurl]").val();
+      }
+      if (bm.color !== $("input[name=bmcolor]").val()) {
+        createdBM.newColor = $("input[name=bmcolor]").val();
+      }
+      if (bm.tags !== $("input[name=bmtags]").val()) {
+        createdBM.newParent = $("input[name=bmtags]").val();
+      }
+      createBM(createdBM, location.reload);
+    });
+    $("#bmEditForm").show();
+  });
+}
+
+// function for create collection form
+
+function createCollectionForm(id) {
+  displayCollect({ id }, (collection) => {
+    $("input[name=collection]").val(collection.name);
+    $("input[name=collectionurl]").val(collection.url);
+    $("input[name=collectioncolor]").val(collection.color);
+    $("input[name=collectiontags]").val(collection.tags);
+    $("#edit-collection").on("submit", (event) => {
+      event.preventDefault();
+      const createdCollect = { id: collection.id };
+      if (bm.name !== $("input[name=bookmark]").val()) {
+        createdCollect.newName = $("input[name=bookmark]").val();
+      }
+      if (bm.url !== $("input[name=bmurl]").val()) {
+        createdCollect.newURL = $("input[name=bmurl]").val();
+      }
+      if (bm.color !== $("input[name=bmcolor]").val()) {
+        createdCollect.newColor = $("input[name=bmcolor]").val();
+      }
+      if (bm.tags !== $("input[name=bmtags]").val()) {
+        createdCollect.newParent = $("input[name=bmtags]").val();
+      }
+      createCollect(createdCollect, location.reload);
+    });
+    $("#collectEditForm").show();
+  });
 }
 
 //Display Functions========================================
@@ -85,23 +171,26 @@ function displayCollect(displaysub) {
 //Edit Functions==========================================
 // TODO: function to edit account info
 // function editAccInfo()
-// TODO: function to edit tab
+
+// function to edit tag
+
 function updateTab(id) {
-  displayTabs({ id }),
-    (tag) => {
-      $("input[name=tag]").val(tag.name);
-      $("input[name=tagcolor]").val(tag.color);
-      $("#tagEditForm").on("submit", (event) => {
-        event.preventDefault();
-        const updatedTag = { id: tag.id };
-        if (tag.name !== $("input[name=tag]").val()) {
-          updatedTag.newName = $("input[name=tag]").val();
-        }
-        if (tag.color !== $("input[name=tagcolor]").val()) {
-          updatedTag.newColor = $("input[name=tagcolor]").val();
-        }
-      });
-    };
+  displayTabs({ id }, (tag) => {
+    $("input[name=tag]").val(tag.name);
+    $("input[name=tagcolor]").val(tag.color);
+    $("#tagEditForm").on("submit", (event) => {
+      event.preventDefault();
+      const updatedTag = { id: tag.id };
+      if (tag.name !== $("input[name=tag]").val()) {
+        updatedTag.newName = $("input[name=tag]").val();
+      }
+      if (tag.color !== $("input[name=tagcolor]").val()) {
+        updatedTag.newColor = $("input[name=tagcolor]").val();
+      }
+      updateTag(updatedTag, location.reload);
+    });
+    $("#tagEditForm").show();
+  });
 }
 
 // function that creates edit page for BM
@@ -241,6 +330,33 @@ function updateCollect(data, cb) {
       method: "PUT",
       url: "/api/collections/parent",
       data: { newParent: data.parent, id: data.id },
+    })
+      .then(cb)
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+}
+
+// function to update tag name and color
+
+function updateTag(data, cb) {
+  if (data.newName) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/collections/name",
+      data: { newName: data.newName, id: data.id },
+    })
+      .then(cb)
+      .fail((err) => {
+        console.log(err);
+      });
+  }
+  if (data.newColor) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/collections/color",
+      data: { newColor: data.color, id: data.id },
     })
       .then(cb)
       .fail((err) => {
