8beae131c469a3520cdf43887207d1df4e41ab9e
Zach <zdspringer99@gmail.com>
2021-02-02 15:01:34 -0800

    clearing the db for futher testing

--- server.js
+++ server.js
@@ -1,31 +1,38 @@
-
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const db = require('./models');
+const db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 2 // 2 hours
-    }
-}))
+      maxAge: 1000 * 60 * 60 * 2, // 2 hours
+    },
+  })
+);
 
 app.use(express.static(__dirname + "/public"));
-app.use("/assets/materialize", express.static(__dirname + "/node_modules/materialize-css/dist"));
-app.use("/assets/jquery", express.static(__dirname + "/node_modules/jquery/dist"));
+app.use(
+  "/assets/materialize",
+  express.static(__dirname + "/node_modules/materialize-css/dist")
+);
+app.use(
+  "/assets/jquery",
+  express.static(__dirname + "/node_modules/jquery/dist")
+);
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -34,25 +41,25 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Controllers
-const userController = require('./controllers/userController');
+const userController = require("./controllers/userController");
 app.use(userController);
 
-const collectionController = require('./controllers/collectionController');
-app.use('/api/collections', collectionController);
+const collectionController = require("./controllers/collectionController");
+app.use("/api/collections", collectionController);
 
-const bookmarkController = require('./controllers/bookmarkController');
-app.use('/api/bookmarks', bookmarkController);
+const bookmarkController = require("./controllers/bookmarkController");
+app.use("/api/bookmarks", bookmarkController);
 
-const tagController = require('./controllers/tagController');
-app.use('/api/tags', tagController);
+const tagController = require("./controllers/tagController");
+app.use("/api/tags", tagController);
 
-app.use(require('./controllers/listRenderer'));
-app.use(require('./controllers/modalRenderer'));
+app.use(require("./controllers/listRenderer"));
+app.use(require("./controllers/modalRenderer"));
 
-require('./routes/api-routes')(app);
+require("./routes/api-routes")(app);
 
 db.sequelize.sync({ force: false }).then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
 });
